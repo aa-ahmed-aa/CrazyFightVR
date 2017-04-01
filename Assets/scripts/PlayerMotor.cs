364a37d70238e9ff1a020fe910c042475fa1ca37
aa-Ahmed-aa <ahmedkhaled36@hotmail.com>
2017-04-01 03:44:01 -0700

    done network with vr camera

---
+++ Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Camera cam;
+
+
+    private Vector3 Velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camerarotation = Vector3.zero;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    //get movement vector
+    public void Move(Vector3 _velocity)
+    {
+
+        Velocity = _velocity;
+
+    }
+
+    //get rotation vector
+    public void Rotate(Vector3 _rotation)
+    {
+
+        rotation = _rotation;
+
+    }
+
+    //get rotation vector for a camera
+    public void RotateCamera(Vector3 _camerarotation)
+    {
+
+        camerarotation = _camerarotation;
+
+    }
+
+
+    //run every physics iteration
+    void FixedUpdate()
+    {
+        preformmovement();
+        performrotation();
+
+    }
+    //preform movement based on velocity variable
+
+    void preformmovement()
+    {
+        if (Velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + Velocity * Time.fixedDeltaTime);
+        }
+
+
+    }
+
+
+    //preform rotation
+    void performrotation()
+    {
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(camerarotation);
+
+        }
+    }
+
+}
---
+++ Assets/scripts/test.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class test : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    private PlayerMotor motor;
+
+
+    void Start()
+    {
+
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //calculate movement velocity as a3d vectors
+        float _xmov = Input.GetAxisRaw("Horizontal");
+        float _zmov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xmov;
+        Vector3 _movVertical = transform.forward * _zmov;
+
+        //final movement vector
+     //   Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+
+        //apply movement
+      //  motor.Move(_velocity);
+
+
+        //calculate rotation as a 3d vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+
+        //apply rotation
+        motor.Rotate(_rotation);
+
+
+
+        /////camera
+
+
+        //calculate camera rotation as a 3d vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _Camerarotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+
+        //apply camera rotation
+        motor.RotateCamera(_Camerarotation);
+
+    }
+
+}
