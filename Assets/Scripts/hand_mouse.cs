52e4c016ad81acccbd8270c0e64bb50ab0c0adcc
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2017-07-04 04:52:00 +0200

    greate milestone fix the test scene errors make the character interact with the cards

---
+++ Assets/Scripts/hand_mouse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hand_mouse : MonoBehaviour {
+    // Add a public field for the Bullet prefab.
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Example());
+    }
+
+    //Add a “Fire” function to fire a Bullet.
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(5.0f);
+        CmdFire();
+    }
+
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 2;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 5.0f);
+    }
+}
---
+++ Assets/Scripts/test_hands_controll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_hands_controll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Enter");
+        //AudioSource.PlayClipPoint(sound, transform.position);
+        if (other.CompareTag("Player"))
+        {
+            transform.Translate(new Vector3(0f, 0f, 5f));
+        }
+    }
+}
