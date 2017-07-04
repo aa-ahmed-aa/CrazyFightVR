5dd891b02f80419b0a003751da5466979245f763
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2017-07-04 03:31:05 +0200

    fix the syncing make the player distinct make the floating details

--- Assets/Scripts/CardsController.cs
+++ Assets/Scripts/CardsController.cs
@@ -6,20 +6,23 @@ public class CardsController : MonoBehaviour {
     public Animator ani;
     public GameObject Character;
     public Transform Character_SpawnPoint;
+    private int counter;
 
     // public AudioClip sound;
 
     // Use this for initialization
     void Start () {
         ani.enabled = false;
+        counter = 0;
     }
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision Enter");
         //AudioSource.PlayClipPoint(sound, transform.position);
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && counter < 1)
         {
+            counter++;
             ani.enabled = true;
             StartCoroutine(Example());
         }
